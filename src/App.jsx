3ee990cfe514f59ce4aa3b8f9ecbe476e05a5ad4
shahinpatwary <shahinhpa93.dev@gmail.com>
2024-02-13 18:32:33 +0600

    code update

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,15 @@ import SectionHeading from './components/ReusableHeading';
 function App() {
 
   return (
-    <>
-     
-        
+    <div className='page-wrapper'>
+
       <SectionHeading
         tagline="About Us"
         SectionTitle="Almost every major <span>economy around</span> the global"
         SectionIntro="SMEs are the backbone of almost every major economy around the globe! SMEs are the backbone of almost every major economy around the globe!"
       />
       
-    </>
+    </div>
   )
 }
 
